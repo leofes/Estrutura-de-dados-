192c9b20a5915e7825532b23e2dceea99a1adbec
leofes <141570379+leofes@users.noreply.github.com>
2026-02-13 01:13:00 +0000

    Sua mensagem de commit

--- CCA220/aula2.c
+++ CCA220/aula2.c
@@ -15,7 +15,7 @@ Celula * criar_celula(int valor){
     Celula *nova = malloc(sizeof(Celula));
     nova -> proximo = NULL;
     nova -> valor = valor;
-    return nova
+    return nova;
 }
 
 LDE *criar_lista(){
@@ -25,6 +25,29 @@ LDE *criar_lista(){
     return nova;
 }
 
+void imprimir(LDE *lista){
+    Celula *atual = lista -> primeiro;
+
+    while(atual != NULL){
+        printf("%d" , atual -> valor);
+        atual = atual -> proximo;
+
+        printf("\n");
+
+    }
+}
+
+void remover(LDE *lista, int valor){
+    if (lista -> primeiro  == NULL){
+        printf("lista vazia");
+    }
+    
+
+}
+
+
+
+//Inserção lista vazia 
 void inserir(LDE *lista, int valor){
     Celula *nova = criar_celula(valor);
     if(lista->qtd == 0){
@@ -37,14 +60,20 @@ void inserir(LDE *lista, int valor){
             anterior = atual;
             atual = atual -> proximo;
         }
+        // Verificando se a inserção é no final da lista
         if(anterior != NULL && atual == NULL){
-            anterior -> proxuno = nova;
+            anterior -> proximo = nova;
             lista -> qtd++;
+        // Verificando se a inserção é no incício da lista
         }else if(anterior == NULL && atual != NULL){
             nova -> proximo = atual;
             lista -> primeiro = nova; 
             lista -> qtd++;
-        }
+        // Verificando se a inserção é no meio da lista
+        }else if(anterior != NULL && atual != NULL)
+            anterior -> proximo = nova;
+            nova -> proximo = atual;
+            lista -> qtd++; 
         
     }
 
@@ -52,6 +81,11 @@ void inserir(LDE *lista, int valor){
 
 
 int main() {
-
+    LDE *lista = criar_lista();
+    int valores[] = {4, 1, 8, 2, 9, 0, 3, 7, 6, 5};
+    for(int i = 0; i < 10; i++){
+        inserir(lista, valores[i]);
+        imprimir(lista);
+    }
     return 0;
 }
